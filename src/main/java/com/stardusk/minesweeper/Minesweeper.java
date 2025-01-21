b26b2a6ac67e734e14a95636baf456e3e23eea1e
tolis <tolis@DESKTOP-N4E6BG6.station>
2025-01-21 22:26:09 +0200

    Started repository

---
+++ src/main/java/com/stardusk/minesweeper/Minesweeper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.stardusk.minesweeper;
+
+/**
+ *
+ * @author tolis
+ */
+public class Minesweeper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
